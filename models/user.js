11a60c6572a075bdb800c9e57e3ed95d5a0742c0
jim-clark <email@jim-clark.com>
2020-07-13 10:07:01 -0700

    Finish Step 1

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const SALT_ROUNDS = 6;
 
 const userSchema = new mongoose.Schema({
   name: String,
@@ -14,6 +17,18 @@ userSchema.set('toJSON', {
     delete ret.password;
     return ret;
   }
+});
+
+userSchema.pre('save', function(next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+  // password has been changed
+  bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash) {
+    if (err) return next(err);
+    // replace the user password with hash
+    user.password = hash;
+    next();
+  });
 });
 
 module.exports = mongoose.model('User', userSchema);
