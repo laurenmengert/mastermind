6ca65cac2c87fec380e703f18d4d6ad8659e92ec
jim-clark <email@jim-clark.com>
2020-07-13 14:59:22 -0700

    Final

--- routes/api/scores.js
+++ routes/api/scores.js
@@ -3,6 +3,14 @@ const router = express.Router();
 const scoresCtrl = require('../../controllers/scores');
 
 router.get('/', scoresCtrl.highScores);
-router.post('/', scoresCtrl.create);
+
+// Protected server routes
+router.post('/', checkAuth, scoresCtrl.create);
+
+/*--- Helper Functions ---*/
+function checkAuth(req, res, next) {
+  if (req.user) return next();
+  return res.status(401).json({msg: 'Not Authorization'});
+}
 
 module.exports = router;
